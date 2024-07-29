a617d2f2bdd250ca4b432026de0346978887c59a
Kawa <kawaoneechan@gmail.com>
2024-07-29 22:13:26 +0200

    timed music

--- ProjectSpecialK/MusicManager.cpp
+++ ProjectSpecialK/MusicManager.cpp
@@ -1,7 +1,7 @@
 #include "SpecialK.h"
 #include "Town.h"
 
-//TODO
+//TODO: make this a Tickable so it can handle transitions and such.
 
 extern Audio* bgm;
 
@@ -17,6 +17,10 @@ void PlayMusic(const std::string& id)
 	if (id == currentID)
 		return;
 
+	tm gm;
+	auto now = time(nullptr);
+	localtime_s(&gm, &now);
+
 	auto entry = library.find(id);
 	if (entry == library.end())
 	{
@@ -26,13 +30,29 @@ void PlayMusic(const std::string& id)
 	}
 
 	auto second = entry->second;
+
+	if (second->IsArray() && second->AsArray()[0]->IsObject())
+	{
+		auto now2 = (gm.tm_hour * 24) + gm.tm_min;
+		auto prev = 0;
+		for (auto& ranges : second->AsArray())
+		{
+			auto r = ranges->AsObject();
+			auto t = GetJSONVec2(r["to"]);
+			auto to = (int)((t[0] * 24) + t[1]);
+			if (now2 < to && now2 > prev)
+			{
+				second = r["file"];
+			}
+			prev = to;
+		}
+	}
+
 	while (second->IsArray())
 	{
 		auto arr = second->AsArray();
 		second = arr[std::rand() % arr.size()];
 	}
-
-	//TODO: Objects with time ranges.
 
 	if (!second->IsString())
 	{
@@ -43,18 +63,30 @@ void PlayMusic(const std::string& id)
 	auto file = second->AsString();
 
 	{
-		tm gm;
-		auto now = time(nullptr);
-		localtime_s(&gm, &now);
 		auto weather = "sunny";
 		if (town.Clouds >= Town::Weather::RainClouds)
 			weather = "rainy";
 		auto tpos = file.find("{time}");
 		if (tpos != std::string::npos)
 			file = file.replace(tpos, 6, fmt::format("{:02}", gm.tm_hour));
-		tpos = file.find("{weather}");
-		if (tpos != std::string::npos)
-			file = file.replace(tpos, 9, weather);
+
+		{
+			auto preWeather = file;
+			tpos = file.find("{weather}");
+			if (tpos != std::string::npos)
+				file = file.replace(tpos, 9, weather);
+			
+			if (EnumerateVFS(file).size() == 0)
+			{
+				//File does not exist. Try sunny weather first.
+				file = preWeather.replace(tpos, 9, "sunny");
+				if (EnumerateVFS(file).size() == 0)
+				{
+					//That too? Okay, try *no* weather!
+					file = preWeather.replace(tpos, 9, "");
+				}
+			}
+		}
 	}
 
 	if (currentFile != file)
