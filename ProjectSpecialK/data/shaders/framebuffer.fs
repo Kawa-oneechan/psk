96843c4713c65930ceb446eff9c5232b85c05556
Kawa <kawaoneechan@gmail.com>
2024-12-30 21:03:54 +0100

    Update framebuffer.fs

--- ProjectSpecialK/data/shaders/framebuffer.fs
+++ ProjectSpecialK/data/shaders/framebuffer.fs
@@ -10,34 +10,50 @@ uniform vec4 spriteColor[200];
 uniform vec4 sourceRect[200];
 uniform bool flipX[200], flipY[200];
 
-const float pixSize = 0.0025;
-
 void main()
 {
-/*
-	//vec4 lol = texture(image, TexCoords);
-	//fragColor = vec4(1.0 - lol.rgb, lol.a);
-	fragColor = texture(image, TexCoords);
+	//Determine pixel size in 0-1
+	vec2 ps = 1.0 / ScreenRes.xy;
 
-	float y = floor(TexCoords.y * ScreenRes.y);
-	if (mod(y, 2.0) == 0.0)
-		fragColor.a = 0.0;
+/*
+	//OUTLINE
+	//-------
+	vec3 c = texture(image, TexCoords).rgb;
+	vec3 n = texture(image, TexCoords - vec2(ps.x, 0)).rgb;
+	vec3 s = texture(image, TexCoords + vec2(ps.x, 0)).rgb;
+	vec3 e = texture(image, TexCoords - vec2(0, ps.y)).rgb;
+	vec3 w = texture(image, TexCoords + vec2(0, ps.y)).rgb;
+	vec3 edge = max(abs(n - s), abs(e - w)) * 4.0;
+	float bri = 1.0 - dot(edge, vec3(0.299, 0.587, 0.114));
+	fragColor = vec4(c * bri, 1.0);
 */
 
 /*
-	float r = texture(image, TexCoords + vec2(pixSize, 0)).r;
+	//SCANLINES
+	//---------
+	fragColor = texture(image, TexCoords) * mod(floor(TexCoords.y * ScreenRes.y), 2.0);
+*/
+
+/*
+	//CHROMATIC ABBERATION
+	//--------------------
+	float r = texture(image, TexCoords + vec2(ps.x * 4.0, 0)).r;
 	float g = texture(image, TexCoords).g;
-	float b = texture(image, TexCoords + vec2(-pixSize, 0)).b;
+	float b = texture(image, TexCoords + vec2(-ps.x * 4.0, 0)).b;
 
 	fragColor = vec4(r, g, b, 1.0);
 */
 
+/*
+	//PIXELATE
+	//--------
 	const vec2 pixelSize = vec2(320.0, 240.0);
-	vec2 uv = floor(TexCoords * pixelSize);
-	uv /= pixelSize;
-	fragColor = texture(image, uv);
+	fragColor = texture(image, floor(TexCoords * pixelSize) / pixelSize);
+*/
 
 /*
+	//FILMGRAIN
+	//---------
 	vec4 color = texture(image, TexCoords);
 	float strength = 16.0;
 	float x = (TexCoords.x + 4.0 ) * (TexCoords.y + 4.0 ) * (TotalTime * 10.0);
