0a7ef3373c5d3e1fbe773c68a49deb73ae6e838f
Kawa <kawaoneechan@gmail.com>
2025-12-09 13:07:09 +0100

    Update model_generic_bottom.fs

--- ProjectSpecialK/data/shaders/model_generic_bottom.fs
+++ ProjectSpecialK/data/shaders/model_generic_bottom.fs
@@ -1,7 +1,7 @@
 	vec3 norm = Toon ? normalize(Normal) : calcNormal(normalVal);
 	vec3 viewPos = ((InvView - model) * vec4(0.0, 0.0, 0.0, 0.1)).xyz;
 	vec3 viewDir = normalize(viewPos - FragPos);
-	float fresnel = getFresnel(model, norm);
+	float fresnel = (fresnelVal > 0.9) ? 0.0 : getFresnel(model, norm);
 
 	vec3 result;
 	result = directLight(norm, albedoVal.rgb, specularVal, viewDir);
